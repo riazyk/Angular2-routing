0d7110a77607a371f717124700ead0aa2291ce69
ryazyk <ryazyk@gmail.com>
2018-04-02 17:39:01 +0300

    Add files via upload

---
+++ src/app/lists/list1/list1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "list-1",
+    templateUrl: "list1.component.html",
+    styleUrls: ["list1.component.css"]
+})
+export class List1Component {
+    
+}
